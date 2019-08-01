840888b49a4c5f7e6136fbc9c43396d6526bbce8
George King <george.w.king@gmail.com>
2019-08-01 11:18:44 -0400

    update to match change in muck environment variable name: MUCK_TARGET -> TARGET.

--- oecd-health/data.sqlite3.sh
+++ oecd-health/data.sqlite3.sh
@@ -1,1 +1,1 @@
-csv-to-sqlite health-expenditure-and-financing.csv.bz2 expenditures life-expectancy.csv.bz2 expectancies -output $MUCK_TARGET
+csv-to-sqlite health-expenditure-and-financing.csv.bz2 expenditures life-expectancy.csv.bz2 expectancies -output $TARGET
